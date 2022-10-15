f738f4adb1eeec4c998af1e89a362142fe9768ab
Nathan <nathanl@outlook.com>
2022-10-15 08:39:38 -0700

    new features in calculator

--- js101/small_problems/calculator.js
+++ js101/small_problems/calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -8,51 +9,53 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator!');
+prompt(MESSAGES['welcome']);
+
+while (true) {
 
 // ask user for 1st number
-prompt("What's the first number?");
-let number1 = readline.question();
+  prompt(MESSAGES['firstNumber']);
+  let number1 = readline.question();
 
-while (invalidNumber(number1)) {
-  prompt("Hmmm...that doesn't look like a valid number.");
-  number1 = readline.question();
-}
+  while (invalidNumber(number1)) {
+    prompt(MESSAGES['invalidNumber']);
+    number1 = readline.question();
+  }
 
 // ask for a 2nd number
-prompt("What's the second number?");
-let number2 = readline.question();
+  prompt(MESSAGES['secondNumber']);
+  let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt("Hmmm...that doesn't look like a valid number.");
-  number2 = readline.question();
-}
+  while (invalidNumber(number2)) {
+    prompt(MESSAGES['invalidNumber']);
+    number2 = readline.question();
+  }
 
 // ask what operation to perform
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+  prompt(MESSAGES['whatOperation']);
+  let operation = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Must choose 1, 2, 3, or 4");
-  operation = readline.question();
-}
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(MESSAGES['invalidOperation']);
+    operation = readline.question();
+  }
 
 // perform the op
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
 
 /* if (operation === '1') { // 1 is addition
   output = Number(number1) + Number(number2);
@@ -65,4 +68,18 @@ switch (operation) {
 } */
 
 // print result to terminal
-prompt(`The result is: ${output}`);
+  prompt(`The result is: ${output}`);
+
+// ask if user wants to do another calculation?
+  prompt(MESSAGES['anotherCalc']);
+  let anotherCalc = readline.question();
+
+  while (!['y', 'n'].includes(anotherCalc)) {
+    prompt(MESSAGES['invalidAnotherCalc']);
+    anotherCalc = readline.question();
+  }
+
+  if (anotherCalc === 'n') {
+    break;
+  }
+}
