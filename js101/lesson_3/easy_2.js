69280817d3f226321973cc0ead1feb5147a2e43d
Nathan <nathanl@outlook.com>
2022-10-17 21:27:11 -0700

    completed easy 2

--- js101/lesson_3/easy_2.js
+++ js101/lesson_3/easy_2.js
@@ -54,3 +54,34 @@ console.log(flintstones);
 
 let question7Flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
 
+let barney = Object.entries(question7Flintstones).filter(pair => pair[0] ==="Barney").shift();
+
+console.log(barney)
+
+// Question 8
+
+let question8Numbers = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+console.log(Array.isArray(question8Numbers));
+console.log(Array.isArray(table));
+
+// Question 9
+
+let title = "Flintstone Family Members";
+
+console.log(title.length); // 25
+console.log((40 - 25) / 2);
+
+let sides = ' '.repeat(Math.floor((40 - title.length) / 2));
+console.log(sides + title + sides);
+
+// Question 10
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+const countTs = (string) => string.split('').filter(char => char === 't').length;
+
+console.log(countTs(statement1));
+console.log(countTs(statement2));
