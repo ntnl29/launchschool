c978f905d33d0985e456eb7c5e834a75258c2e11
Nathan <nathanl@outlook.com>
2022-10-19 16:21:16 -0700

    madlibs

--- js101/exercises/madlibs.js
+++ js101/exercises/madlibs.js
@@ -7,12 +7,18 @@ const MESSAGES = {
   getAdverb: "Enter an adverb"
 };
 
-let noun;
-let verb;
-let adjective;
-let adverb;
-
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+let noun = readline.question(prompt(MESSAGES.getNoun));
+
+let verb = readline.question(prompt(MESSAGES.getVerb));
+
+let adjective = readline.question(prompt(MESSAGES.getAdjective));
+
+let adverb = readline.question(prompt(MESSAGES.getAdverb));
+
+console.log(`Do you ${verb} your ${adjective} ${noun} ${adverb}? That's hilarious!`);
+console.log(`The ${adjective} ${noun} ${verb} ${adverb} over the lazy ${noun}.`);
+console.log(`The ${noun} ${adverb} ${verb} up ${adjective} Joe's turtle.`);
