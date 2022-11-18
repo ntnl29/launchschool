fae0cdc50fa4bfa6f8f7e49440a364556d760906
ntnl <nathanl@outlook.com>
2022-11-17 17:26:12 -0800

    thu update

--- js101/exercises/string_and_text_processing/9_search_word_pt_1.js
+++ js101/exercises/string_and_text_processing/9_search_word_pt_1.js
@@ -1,5 +1,16 @@
+// function searchWord(string, text) {
+//   return text.split(' ').filter(word => word
+//    .toLowerCase() === string.toLowerCase()).length;
+// }
+
 function searchWord(string, text) {
-  
+  let count = 0;
+  text.split(' ').forEach(word => {
+    if (word.toLowerCase() === string.toLowerCase()) {
+      count += 1;
+    }
+  });
+  return count;
 }
 
 const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
