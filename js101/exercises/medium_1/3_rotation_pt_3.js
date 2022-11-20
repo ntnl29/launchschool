2fb0f0c494d80e89fe4d68fc2fba60970447c9d5
Nathan <nathanl@outlook.com>
2022-11-20 15:26:02 -0800

    sun update

--- js101/exercises/medium_1/3_rotation_pt_3.js
+++ js101/exercises/medium_1/3_rotation_pt_3.js
@@ -21,7 +21,11 @@ rules:
 */
 
 function maxRotation(number) {
-  for (let count = 0; count < number.length; count -= 1)
+  let length = String(number).length;
+  for (let count = length; count >= 2; count -= 1) {
+    number = rotateRightmostDigits(number, count);
+  }
+  return number;
 }
 
 function rotateRightmostDigits(number, count) {
@@ -37,8 +41,8 @@ function rotateString(string) {
   return string.slice(1) + string[0];
 }
 
-maxRotation(735291);          // 321579
-maxRotation(3);               // 3
-maxRotation(35);              // 53
-maxRotation(105);             // 15 -- the leading zero gets dropped
-maxRotation(8703529146);      // 7321609845
+console.log(maxRotation(735291));          // 321579
+console.log(maxRotation(3));               // 3
+console.log(maxRotation(35));              // 53
+console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
+console.log(maxRotation(8703529146));      // 7321609845
