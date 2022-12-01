a91327c946d92c5d3d97e61820e969e9b86b9109
ntnl <nathanl@outlook.com>
2022-11-30 20:53:38 -0800

    wed update

--- js101/lesson_6/twenty_one .js
+++ js101/lesson_6/twenty_one .js
@@ -20,8 +20,8 @@ Deck: Start with a standard 52-card deck consisting of:
 - 13 values (2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace)
 */
 
-const deckSuits = ['H', 'D', 'C', 'S'];
-const deckValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const deckSuits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const deckValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 const deck = shuffle(createDeck(deckSuits, deckValues));
 
 const playerCards = [];
@@ -39,18 +39,6 @@ function printIntro() {
   console.log("");
   prompt('"RULES PLACEHOLDER"'); // Write out rules
   console.log("-".repeat(15));
-}
-
-function printHands(playerCards, dealerCards) {
-  prompt('The dealer shuffles the cards. He slowly passes out the cards.');
-  prompt('One for you, one for him. Another for you, another for him.');
-  prompt('Both of your cards are faced upwards. One of his cards faces upwards and the other is face down');
-  console.log("");
-  prompt('As you turn your eyes towards your cards, you see:');
-  console.log((' ').repeat(5) + `${playerCards}`); // player's cards
-  console.log("");
-  prompt('You glance over to the dealer\'s cards and you see:');
-  console.log((' ').repeat(5) + `${dealerCards[0]} and UNKNOWN CARD`); // dealer's cards
 }
 
 function createDeck(deckValues, deckSuits) {
@@ -71,15 +59,60 @@ function shuffle(deck) {
   return deck;
 }
 
-function dealTheCards(deck) {
+function dealDeck(deck) {
   for (let index = 0; index < 2; index += 1) {
     playerCards.push(deck.shift());
     dealerCards.push(deck.shift());
   }
 }
 
+function printHands(playerCards, dealerCards) {
+  prompt('The dealer shuffles the cards. He slowly passes out the cards.');
+  prompt('One for you, one for him. Another for you, another for him.');
+  prompt('Both of your cards are faced upwards. One of his cards faces upwards and the other is face down.');
+  console.log("");
+  prompt('You glance over to the dealer\'s cards and you see:');
+  console.log((' ').repeat(5) + `${dealerCards[0][0]} and an uncertain card`); // dealer's cards
+  console.log("");
+  prompt('As you turn your eyes towards your cards, you see:');
+  console.log((' ').repeat(5) + translateHands(playerCards)); // player's cards
+}
+
+function translateHands(cards) {
+  return `${cards[0][0]} and ${cards[1][0]}`;
+}
+
+function printTotal(cards) {
+  console.log('');
+  prompt('Your total is: ' + total(cards));
+}
+
+function total(cards) {
+  // cards = [['H', '3'], ['S', 'Q'], ... ]
+  let values = cards.map(card => card[0]);
+
+  let sum = 0;
+  values.forEach(value => {
+    if (value === "Ace") {
+      sum += 11;
+    } else if (['Jack', 'Queen', 'King'].includes(value)) {
+      sum += 10;
+    } else {
+      sum += Number(value);
+    }
+  });
+
+  // correct for Aces
+  values.filter(value => value === "Ace").forEach(_ => {
+    if (sum > 21) sum -= 10;
+  });
+
+  return sum;
+}
+
 // BODY
 
 printIntro();
-dealTheCards(deck);
+dealDeck(deck);
 printHands(playerCards, dealerCards);
+printTotal(playerCards);
