1a0c6c335503d83c728da736e334ed83c7979c06
Nathan <nathanl@outlook.com>
2022-12-10 15:21:23 -0800

    sat update

--- js101/exercises/medium_2/1_lettercase_percentage_ratio.js
+++ js101/exercises/medium_2/1_lettercase_percentage_ratio.js
@@ -1,12 +1,22 @@
 function letterPercentages(string) {
+  let total = string.length;
 
+  function percent(regex) {
+    let matchChars = string.match(regex) || [];
+    return ((matchChars.length / total) * 100).toFixed(2);
+  }
+  return {
+    lowercase: percent(/[a-z]/g),
+    uppercase: percent(/[A-Z]/g),
+    neither: percent(/[^a-z]/gi)
+  };
 }
 
-letterPercentages('abCdef 123');
+console.log(letterPercentages('abCdef 123'));
 // { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
 
-letterPercentages('AbCd +Ef');
+console.log(letterPercentages('AbCd +Ef'));
 // { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
 
-letterPercentages('123');
+console.log(letterPercentages('123'));
 // { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
---
+++ js101/exercises/medium_2/2_triangle_sides.js
@@ -0,0 +1,30 @@
+// function triangle(sideOne, sideTwo, sideThree) {
+//   let perimeter = sideOne + sideTwo + sideThree;
+//   let longest = Math.max(sideOne, sideTwo, sideThree);
+//   let shortest = Math.min(sideOne, sideTwo, sideThree);
+//   let middle = perimeter - longest - shortest;
+
+//   if (shortest === 0) {
+//     return 'invalid';
+//   } else if (longest > (middle + shortest)) {
+//     return 'invalid';
+//   } else if (sideOne === sideTwo && sideTwo === sideThree) {
+//     return 'equilateral';
+//   } else if (sideOne === sideTwo || sideTwo === sideThree) {
+//     return 'isoceles';
+//   } else {
+//     return 'scalene';
+//   }
+// }
+
+function triangle(sideOne, sideTwo, sideThree) {
+  let sortedArray = [sideOne, sideTwo, sideThree].sort((a, b) => b - a);
+
+  
+}
+
+console.log(triangle(3, 3, 3));        // "equilateral"
+console.log(triangle(3, 3, 1.5));      // "isosceles"
+console.log(triangle(3, 4, 5));        // "scalene"
+console.log(triangle(0, 3, 3));        // "invalid"
+console.log(triangle(3, 1, 1));        // "invalid"
