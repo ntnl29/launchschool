9d45e7ee3f457ff6db0bd7e7dc484843507100bb
ntnl <nathanl@outlook.com>
2022-12-09 21:16:25 -0800

    fri updates

--- js101/exercises/medium_1/7_fibonacci_numbers_procedural.js
+++ js101/exercises/medium_1/7_fibonacci_numbers_procedural.js
@@ -7,6 +7,12 @@ function fibonacci(num) {
   return array[1];
 }
 
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
 console.log(fibonacci(20));       // 6765
 console.log(fibonacci(50));       // 12586269025
 console.log(fibonacci(75));       // 2111485077978050
---
+++ js101/exercises/medium_2/1_lettercase_percentage_ratio.js
@@ -0,0 +1,12 @@
+function letterPercentages(string) {
+
+}
+
+letterPercentages('abCdef 123');
+// { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
+
+letterPercentages('AbCd +Ef');
+// { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+
+letterPercentages('123');
+// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
