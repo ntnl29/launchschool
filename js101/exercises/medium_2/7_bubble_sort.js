843f832594a9406ecbe4d3babd4d2deb200e2541
ntnl <nathanl@outlook.com>
2022-12-17 14:30:57 -0800

    sat

---
+++ js101/exercises/medium_2/7_bubble_sort.js
@@ -0,0 +1,28 @@
+/*
+input: array
+output: mutated array
+*/
+
+function bubbleSort(array) {
+  while (true) {
+    let swapped = false;
+    for (let index = 1; index < array.length; index += 1) {
+      if (array[index - 1] <= array[index]) continue;
+      [array[index - 1], array[index]] = [array[index], array[index - 1]];
+      swapped = true;
+    }
+    if (!swapped) break;
+  }
+}
+
+let array1 = [5, 3];
+bubbleSort(array1);
+console.log(array1);    // [3, 5]
+
+let array2 = [6, 2, 7, 1, 4];
+bubbleSort(array2);
+console.log(array2);    // [1, 2, 4, 6, 7]
+
+let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+bubbleSort(array3);
+console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
