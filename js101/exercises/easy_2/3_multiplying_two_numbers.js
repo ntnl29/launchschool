96e9cf8931ff9a88f7187a3a8117eeb54021db37
ntnl <nathanl@outlook.com>
2022-12-20 14:50:06 -0800

    review

---
+++ js101/exercises/easy_2/3_multiplying_two_numbers.js
@@ -0,0 +1,8 @@
+/*
+Create a function that takes two arguments, multiplies them
+together, and returns the result.
+*/
+
+const multiply = (num1, num2) => num1 * num2;
+
+console.log(multiply(5, 3) === 15); // logs true
