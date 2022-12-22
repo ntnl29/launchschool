46c5e7d24e398a66fac9f7812499393f9152da5e
ntnl <nathanl@outlook.com>
2022-12-21 22:05:39 -0800

    review

--- js101/exercises/easy_2/easy_2.js
+++ js101/exercises/easy_2/easy_2.js
@@ -77,6 +77,25 @@ console.log(oddities([])); // logs []
 
 // Convert a String to a Number!
 
+/*
+The parseInt() method converts a string of numeric characters (including an
+optional plus or minus sign) to an integer. The method takes 2 arguments
+where the first argument is the string we want to convert and the second
+argument should always be 10 for our purposes. parseInt() and the Number()
+method behave similarly. In this exercise, you will create a function that
+does the same thing.
+
+Write a function that takes a string of digits and returns the appropriate
+number as an integer. You may not use any of the methods mentioned above.
+
+For now, do not worry about leading + or - signs, nor should you worry about
+invalid characters; assume all characters will be numeric.
+
+You may not use any of the standard conversion methods available in JavaScript,
+such as String() and Number(). Your function should do this the old-fashioned
+way and calculate the result by analyzing the characters in the string.
+*/
+
 const stringToInteger = str => str *= 1;
 
 console.log(stringToInteger("4321") === 4321); // logs true
