f3c52fd05b46846f4fd4db2e022c7444412d9d1a
ntnl <nathanl@outlook.com>
2022-12-21 22:05:19 -0800

    review

---
+++ js101/exercises/easy_3/problems.js
@@ -0,0 +1,20 @@
+/*
+ddaaiillyy ddoouubbllee
+
+Write a function that takes a string argument and returns a new string that
+contains the value of the original string with all consecutive duplicate
+characters collapsed into a single character.
+*/
+
+crunch('ddaaiillyy ddoouubbllee');    // "daily double"
+crunch('4444abcabccba');              // "4abcabcba"
+crunch('ggggggggggggggg');            // "g"
+crunch('a');                          // "a"
+crunch('');                           // ""
+
+/*
+Bannerizer
+
+Write a function that will take a short line of text, and write it to the
+console log within a box.
+*/
